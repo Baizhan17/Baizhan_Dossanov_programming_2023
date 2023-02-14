62409f84f70bc919825acc998399db4ee0efe4a1
Baizhan17 <70335254+Baizhan17@users.noreply.github.com>
2023-02-14 07:21:34 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.ProcessFileButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FilePathTextBox
+            // 
+            this.FilePathTextBox.Location = new System.Drawing.Point(280, 98);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.Size = new System.Drawing.Size(100, 22);
+            this.FilePathTextBox.TabIndex = 0;
+            // 
+            // ProcessFileButton
+            // 
+            this.ProcessFileButton.Location = new System.Drawing.Point(158, 96);
+            this.ProcessFileButton.Name = "ProcessFileButton";
+            this.ProcessFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcessFileButton.TabIndex = 1;
+            this.ProcessFileButton.Text = "Process";
+            this.ProcessFileButton.UseVisualStyleBackColor = true;
+            this.ProcessFileButton.Click += new System.EventHandler(this.ProcessFileButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(296, 200);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(48, 17);
+            this.ResultLabel.TabIndex = 2;
+            this.ResultLabel.Text = "Result";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(280, 148);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.OutputTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.ProcessFileButton);
+            this.Controls.Add(this.FilePathTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.Button ProcessFileButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace Reports1
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:testsqlservermj.database.windows.net,1433;Initial Catalog = ProgramminClasses; Persist Security Info = False; UserID = dbadmin; Password = Qwer!234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate =False; Connection Timeout = 30; ");
+        }
+    }
+
+    public class Customer
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string companyName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context = new CustomerContext())
+            {
+                var customers = context.Customers
+                    .Where(c => c.FirstName == "Baizhan" && c.LastName == "Dossanov" && c.CompanyName == "Samsung")
+                    .ToList();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.firstName} {customer.lastName} from {customer.companyName}");
+                }
+            }
+            //using (var context = new CustomerContext(new DbContextOptionsBuilder<CustomerContext>().UseSqlServer(@"Server=tcp:testsqlservermj.database.windows.net,1433;Initial Catalog = ProgramminClasses; Persist Security Info = False; UserID = dbadmin; Password = Qwer!1234; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate =False; Connection Timeout = 30; " ));
+            //{
+            //    var customer = new Customer { firstName = FirstName, lastName = LastName, companyName = CompanyName };
+            //    context.Customers.Add(customer);
+            //    context.SaveChanges();
+            //}
+        }
+    }
+}
